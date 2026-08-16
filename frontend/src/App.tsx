776b6f9d34fdb700693f93b1fb031e3187ac5d06
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-08-16 09:00:32 +0200

    [CHANGED]: Updated for auth context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,25 @@ import appMUITheme from "./style/theme";
 import PBasePage from "./pages/shared/PBasePage";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { EAppMenus, ROUTE_AUTH } from "./consts";
-import PAuth from "./pages/PAuth/PAuth";
+import PAuth from "../rgt/pages/PAuth/PAuth";
+import CAuthContext from "../rgt/context/auth/CAuthContext";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<ThemeProvider theme={appMUITheme}>
 				<CssBaseline />
-				<Routes>
-					<Route path={ROUTE_AUTH} element={<PAuth />} />
-					<Route element={<PBasePage />}>
-						<Route index />
-						<Route path={EAppMenus.MANAGEMENT_TODO} />
-						<Route path={EAppMenus.MANAGEMENT_ROADMAP} />
-						<Route path={EAppMenus.MANAGEMENT_BUGS} />
-					</Route>
-				</Routes>
+				<CAuthContext>
+					<Routes>
+						<Route path={ROUTE_AUTH} element={<PAuth />} />
+						<Route element={<PBasePage />}>
+							<Route index />
+							<Route path={EAppMenus.MANAGEMENT_TODO} />
+							<Route path={EAppMenus.MANAGEMENT_ROADMAP} />
+							<Route path={EAppMenus.MANAGEMENT_BUGS} />
+						</Route>
+					</Routes>
+				</CAuthContext>
 			</ThemeProvider>
 		</BrowserRouter>
 	);
