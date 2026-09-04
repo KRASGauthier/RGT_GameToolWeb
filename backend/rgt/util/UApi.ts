4c9536cd16ca3db271c76da265eb9bf7cb4e4a59
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-09-04 17:46:19 +0200

    [CHANGED]: Upgraded apiChecker

--- backend/rgt/util/UApi.ts
+++ backend/rgt/util/UApi.ts
@@ -1,16 +1,40 @@
 import { TAPIChecker } from "../types/api/TAPI.js";
 
 export function checkApiSub(data: Record<string, unknown>, checker: TAPIChecker) {
+	for (const [key] of Object.entries(data)) {
+		if (!(key in checker))
+			throw {
+				code: 400,
+				message: "Contract missmatch",
+				log: `Unexpected field: ${key}`,
+			};
+	}
+
 	Object.entries(checker).forEach(([key, value]) => {
-		if (!(key in data))
-			throw { code: 400, message: "Contract missmatch", log: `Missing field: ${key}` };
-		else if (value.type != "checker" && typeof data[key] != value.type)
+		if (!(key in data) && !value.optional)
+			throw {
+				code: 400,
+				message: "Contract missmatch",
+				log: `Missing field: ${key}`,
+			};
+
+		if (!(key in data) && value.optional) return;
+
+		if (value.type != "checker" && value.type != "array" && typeof data[key] != value.type)
 			throw {
 				code: 400,
 				message: "Contract missmatch",
 				log: `Wrong field type: ${key} (got: ${typeof data[key]}, expected: ${value.type})`,
 			};
-		else if (value.type == "checker" && value.checker)
+
+		if (value.type == "array" && !Array.isArray(data[key]))
+			throw {
+				code: 400,
+				message: "Contract missmatch",
+				log: `Wrong field type: ${key} (got: ${typeof data[key]}, expected: array)`,
+			};
+
+		if (value.type == "checker" && value.checker)
 			checkApiSub(data[key] as Record<string, unknown>, value.checker);
 	});
 }
