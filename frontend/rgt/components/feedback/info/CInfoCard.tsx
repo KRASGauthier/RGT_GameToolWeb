9b02937eb0b9124cf2775e7e79207f7767770465
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-09-11 08:54:25 +0200

    [ADDED]: Default app card info (eg: Loading...}

---
+++ frontend/rgt/components/feedback/info/CInfoCard.tsx
@@ -0,0 +1,21 @@
+import { Stack } from "@mui/material";
+import type { GCompProps } from "../../shared/ccommon";
+import CCard from "../../surfaces/CCard";
+import CText from "../../text/CText";
+import type { ReactNode } from "react";
+import type { TCardStyling } from "../../../style/components/surfaces/CCardStyle";
+
+export interface CInfoCardProps extends GCompProps {
+	cardStyling?: TCardStyling;
+	children: string | ReactNode
+}
+
+function CInfoCard({cardStyling, children}: CInfoCardProps) {
+	return <Stack direction="column" sx={{ mt: "20px", alignItems: "center" }}>
+		<CCard styling={cardStyling}>
+			{typeof children == "string" ? <CText>{children}</CText> : children}
+		</CCard>
+	</Stack>
+}
+
+export default CInfoCard;
