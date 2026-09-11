e05ce6558790d3d2e05f5c6813a0b146d8bbfa91
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-09-11 08:53:21 +0200

    [ADDED]: UImage utils file

---
+++ backend/rgt/util/UImages.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+import { STATIC_IMAGES } from "../../src/consts.js";
+
+export function makePath(...paths: string[]): string {
+	let out: string = process.env.BACKEND_UPLOADE_LOCATION ?? "/home/app/uploaded-dev";
+	paths.forEach((path: string) => {
+		out += path;
+	})
+
+	if(!fs.existsSync(out))
+		fs.mkdirSync(out, {recursive: true})
+
+	return out;
+}
+
+export function makeFrontPath(...paths: string[]): string {
+	let out: string = STATIC_IMAGES
+	paths.forEach((path: string) => {
+		out += path;
+	})	
+	return out;
+}
